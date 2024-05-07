84525fdbaa085b5252f914de48ef298b6294cf69
samiul423 <mdsamiul4123@gmail.com>
2024-05-07 10:25:08 +0600

    code addwd

---
+++ First.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class First {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n=18;
+        if(n>18){
+            System.out.println("Adult");
+        }else{
+            System.out.println("Not Adult");
+        }
+    }
+}
